915618267dcc6e15c28f7525636eb2442ade3d6f
Elizabeth Griffith <elizabethgriffith97@gmail.com>
2022-05-08 16:54:33 -0400

    Complete lesson 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(...args) {
+    const array = args[0]
+    const newArray = []
 
+    array.forEach((el) => {
+        if(!args.includes(el)){
+            newArray.push(el)
+        }
+    })
+
+    return newArray
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
