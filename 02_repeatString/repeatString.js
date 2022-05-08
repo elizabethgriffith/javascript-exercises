4f8dd7b339e3d1f392b57e16eb1fac145933f5ba
Elizabeth Griffith <elizabethgriffith97@gmail.com>
2022-05-08 16:13:30 -0400

    Complete lesson 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
+//- You're going to want to use a loop for this one.
 
+//- Create a variable to hold the string you're going to return, create a loop that repeats the given number of times and add the given string to the result on each loop.
+
+
+const repeatString = function(string, num) {
+    let repeatedString = ""
+    if ( num < 0){
+        return 'ERROR'
+    }
+    for (let i = 0 ; i < num ; i++){
+        repeatedString = repeatedString + string
+    }
+    return repeatedString
 };
 
 // Do not edit below this line
