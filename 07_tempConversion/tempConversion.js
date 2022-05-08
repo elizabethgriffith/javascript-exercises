566bebe211bc86b87756945f844212e38dde9d5f
Elizabeth Griffith <elizabethgriffith97@gmail.com>
2022-05-08 17:49:34 -0400

    Complete lesson 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(fah) {
+  let newCel = (fah - 32) * 5/9
+  return Number(newCel.toFixed(1))
 };
 
-const ctof = function() {
-
+const ctof = function(cel) {
+  let newFah = (cel*9/5 + 32)
+  return Number(newFah.toFixed(1))
 };
 
 // Do not edit below this line
